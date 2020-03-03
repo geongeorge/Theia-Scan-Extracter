c49b11348ea3511dee2a1062d1e6140c4107c3e4
Geon George <geongeorge98@gmail.com>
2020-03-03 10:32:51 +0530

    added image flag

--- i.py
+++ i.py
@@ -1,15 +1,41 @@
 import argparse
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-
-ap.add_argument("-s", "--sample", required = False, action='store_true',
-	help = "Get a sample image before exporting")
+__author__ = "Geon George"
+__version__ = "0.1.0"
+__license__ = "MIT"
 
 
-ap.add_argument("-t", '--threshold', type=int, action='store',required = True,
-	help = "Set threshold value [0-255]")
+def main():
+    """ Main entry point of the app """
 
-args = vars(ap.parse_args())
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
 
-# args['sample'] and args['threshold']
+    ap.add_argument("-s", "--sample", required=False, action='store_true',
+                    help="Get a sample image before exporting")
+
+    ap.add_argument("-t", '--threshold', type=int, action='store', required=True,
+                    help="Set threshold value [0-255]")
+
+    ap.add_argument("-i", '--image', type=int, action='store', required=True,
+                    help="Set image file")
+
+    args = vars(ap.parse_args())
+
+    # args['sample'], args['image'] and args['threshold']
+
+    # Image file name (and location)
+    image = args['threshold']
+
+    # Check if file exist
+    if(not os.path.isfile(image)):
+        print("Cannot locate image: "+image)
+        return
+
+    # Proceed to process the image
+    # processImage(image)
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
